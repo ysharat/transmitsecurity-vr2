e9a043c1dfc384c8a0e1ac603a4e0c392a2f8452
sharath <ysharat@gmail.com>
2024-06-18 19:11:59 -0400

    commit-3

--- transmit_security_App_react-2/src/App.jsx
+++ transmit_security_App_react-2/src/App.jsx
@@ -22,7 +22,7 @@ import { ErrorElement } from './components';
 // actions
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
-import { action as forgotpassword } from './pages/ForgotPassword';
+import { action as forgotpassword } from './pages/Forgotpassword';
 import { action as changepassword } from './pages/Changepassword';
 
 import { store } from './store';
